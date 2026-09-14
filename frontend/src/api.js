5ea0fb023cc31ea69dcd89ab0184b077412fcd10
muzammil6786 <khanmuzammil6786@gmail.com>
2026-09-14 18:26:55 +0530

    Dreamstage frontend url update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://dreamstage-3heh.onrender.com/api"
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("ds_token");
